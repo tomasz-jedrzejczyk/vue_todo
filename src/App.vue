9e2333178f197280d1fb6942ad3b6d907a375771
tomasz-jedrzejczyk <tomasz.jedrzejczyk.dev@gmail.com>
2020-09-28 22:43:37 +0200

    first commit of new project

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+
+  <div class="task">
+    <p>Nowe zadanie: {{ newTask }}</p>
+    
+    <input 
+      type="text" 
+      placeholder="Wprowadz zadanie" 
+      v-model="newTask"
+    >
+    <button 
+     @click="addTask"
+    >Dodaj</button>
+  </div>
+
+
+
+  <div class="task" v-for="task in tasks" v-bind:key="task.id">
+    <h2>{{ task.title }}</h2>
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        newTask: '',
+        tasks: [
+          { title: 'Umyć samochód', completed: false, id: 1 },
+          { title: 'Wyprowadzić psa', completed: false, id: 2 }
+        ]
+      }
+    },
+    methods: {
+      addTask() {
+        this.tasks.push({
+          title: this.newTask, 
+          completed: false, 
+          id: Math.random 
+        })
+        this.newTask = ''
+      }
+    }
+  }
+</script>
+
+<style>
+  .task {
+    border: 1px solid #cdcdcd;
+    margin: 8px;
+    padding: 10px;
+  }
+</style>
